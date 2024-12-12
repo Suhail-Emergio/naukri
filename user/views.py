9c680de18c6fc449fe7a223b071e17029dc6546c
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2024-12-12 13:54:28 +0530

    Added descriptions to openapi docs

--- user/views.py
+++ user/views.py
@@ -11,11 +11,12 @@ from ninja.responses import codes_4xx
 from ninja_jwt.authentication import AsyncJWTAuth
 from asgiref.sync import sync_to_async
 from ninja_jwt.tokens import RefreshToken, AccessToken
+from django.contrib.auth.hashers import check_password
 
 user_api = Router(tags=['user'])
 User = get_user_model()
 
-@user_api.post("/register", auth=None, response={201: TokenSchema, 409: Message})
+@user_api.post("/register", auth=None, response={201: TokenSchema, 409: Message}, description="User creation")
 async def register(request, data: UserCreation):
     if not await User.objects.filter(Q(username=data.username) | Q(email=data.email)).aexists():
         user = await User.objects.acreate(**data.dict())
@@ -25,15 +26,24 @@ async def register(request, data: UserCreation):
         return 201, {'access': str(refresh.access_token), 'refresh': str(refresh)}
     return 409, {"message": "User already exists"}
 
-@user_api.post("/login", auth=None, response={200: TokenSchema, 401: Message})
-async def login(request, data: MobileLogin):
+@user_api.post("/mobile_login", auth=None, response={200: TokenSchema, 401: Message}, description="Authenticate user with username and password")
+async def mobile_login(request, data: MobileLogin):
     user = await sync_to_async(authenticate)(username=data.username, password=data.password)
     if not user:
         return 401, {"message": "Invalid credentials"}
     refresh = RefreshToken.for_user(user)
     return 200, {'access': str(refresh.access_token), 'refresh': str(refresh)}
 
-@user_api.post("/refresh", auth=None, response={200: TokenSchema, 401: Message})
+@user_api.post("/email_login", auth=None, response={200: TokenSchema, 401: Message}, description="Authenticate user with email and password")
+async def email_login(request, data: EmailLogin):
+    if await User.objects.filter(email=data.email).aexists():
+        user = await User.objects.aget(email=data.email)
+        if check_password(data.password, user.password):
+            refresh = RefreshToken.for_user(user)
+            return 200, {'access': str(refresh.access_token), 'refresh': str(refresh)}
+    return 401, {"message": "Invalid credentials"}
+
+@user_api.post("/refresh", auth=None, response={200: TokenSchema, 401: Message}, description="Refresh AccessToken using RefreshToken")
 def refresh_token(request, token_data: TokenRefreshSchema):
     try:
         refresh = RefreshToken(token_data.refresh)
@@ -41,7 +51,7 @@ def refresh_token(request, token_data: TokenRefreshSchema):
     except Exception:
         return 401, {"message": "Invalid refresh token"}
 
-@user_api.get("/", response={200: UserData, 401: Message})
+@user_api.get("/", response={200: UserData, 401: Message}, description="Get info of logged user")
 async def user(request):
     user = request.user
     return 200, user
