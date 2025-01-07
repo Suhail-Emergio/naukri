9723d12b1715730a2ad3dda9ad957dbc79569e50
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-07 10:19:59 +0530

    Update languages_data view to return formatted language list or error message

--- seeker/details/views.py
+++ seeker/details/views.py
@@ -119,7 +119,10 @@ async def languages_data(request):
     if await Personal.objects.filter(user=request.auth).aexists():
         personal = await Personal.objects.aget(user=request.auth)
         language = await sync_to_async(lambda: personal.languages)()
-        return 200, language
+        if language:
+            languages_list = [LanguageData(**lang) for lang in language.values()]
+            return 200, languages_list
+        return 409, {"message": "No language data found"}
     return 404, {"message": "Personal data not found"}
 
 @details_api.patch("/update_language", response={201: Message, 404: Message, 403: Message, 409: Message}, description="User language data update")
