d72d23442cb34bb8641acefbd3a1fd3b7d5367f1
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-09 18:37:26 +0530

    update apps.py

--- jobs/job_actions/apps.py
+++ jobs/job_actions/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class JobActionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'job_actions'
+
+    
+    def ready(self):
+        import jobs.job_actions.signals
