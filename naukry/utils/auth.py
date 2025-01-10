415de7fb76dc9e090f79912f7e0017f4b7c02de7
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-10 09:36:20 +0530

    update auth.py

--- naukry/utils/auth.py
+++ naukry/utils/auth.py
@@ -5,14 +5,9 @@ from django.conf import settings
 
 class AsyncJWTAuth(HttpBearer):
     async def authenticate(self, request, token):
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-            user_id = payload.get('user_id')
-            if user_id and await UserProfile.objects.filter(id=user_id).aexists():
-                user = await UserProfile.objects.aget(id=user_id)
-                return user
-            return None
-        except jwt.ExpiredSignatureError:
-            return None
-        except Exception as e:
-            return None
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        user_id = payload.get('user_id')
+        if await UserProfile.objects.filter(id=user_id).aexists():
+            user = await UserProfile.objects.aget(id=user_id)
+            return user
+        return None
