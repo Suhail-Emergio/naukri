e8e1e4e14fc8de96c144e6119e399d33025abc0d
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-10 14:46:33 +0530

    update twilio.py

--- naukry/utils/twilio.py
+++ naukry/utils/twilio.py
@@ -18,13 +18,14 @@ def send_otp(otp, number):
 
 def send_updates(body, number):
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    variables = {
+        "1": body
+    }
     message = client.messages.create(
         content_sid="HX2c2d60626811876c3f71b2112405047d",
         from_='whatsapp:+917594088814',
         to=f'whatsapp:+91{number}',
-        content_variables=json.dumps({
-            "1": body
-        }),    
+        content_variables=variables,
     )
     print(f"Message sent with SID: {message.sid}")
 
