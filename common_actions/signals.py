7e679ff749f29d9f05496b7a8ec382773bba5cf7
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-15 14:52:39 +0530

    update signals.py

--- common_actions/signals.py
+++ common_actions/signals.py
@@ -1,18 +1,19 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.db.models import Count
-from .models import Notification, Invitation
+from common_actions.models import Notification
+from recruiter.recruiter_actions.models import InviteCandidate
 import httpx
 from django.conf import settings
 import asyncio
 
 @receiver([post_save, post_delete], sender=Notification)
-@receiver([post_save, post_delete], sender=Invitation)
+@receiver([post_save, post_delete], sender=InviteCandidate)
 def update_counts(sender, instance, **kwargs):
     async def async_update():
         user = instance.user
         notification_count = await Notification.objects.filter(user=user, read=False).acount()
-        invitation_count = await Invitation.objects.filter(user=user, status='pending').acount()
+        invitation_count = await InviteCandidate.objects.filter(user=user, status='pending').acount()
         async with httpx.AsyncClient() as client:
             await client.post(
                 f"{settings.WEBSOCKET_URL}/update_counts",
