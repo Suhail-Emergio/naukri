7514f9c6181c46d71a443126c5f74963c9515d55
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-16 14:57:17 +0530

    update signals.py

--- common_actions/signals.py
+++ common_actions/signals.py
@@ -5,7 +5,7 @@ from common_actions.models import Notification
 from recruiter.recruiter_actions.models import InviteCandidate
 import httpx
 from django.conf import settings
-from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync
 from ninja_jwt.tokens import RefreshToken, AccessToken
 from web_sockets.main import manager
 
@@ -25,4 +25,4 @@ def update_counts(sender, instance, **kwargs):
         },
     }
     print(message)
-    sync_to_async(manager.broadcast_to_user)(message=message, user_id=user.id)
+    async_to_sync(manager.broadcast_to_user)(message=message, user_id=user.id)
