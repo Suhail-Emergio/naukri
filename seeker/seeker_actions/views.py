912a479735d0306d0594892f43053710bcfb74bf
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-16 15:48:07 +0530

    update views.py

--- seeker/seeker_actions/views.py
+++ seeker/seeker_actions/views.py
@@ -130,7 +130,12 @@ def seach_apps(request, type: str = ""):
     appearences = 0
     if type == "week":
         start_date = today - timedelta(days=7) 
-        appearences = (SearchApps.objects.filter(user=user, date__range=[start_date, today]).annotate(date=TruncDate("date")).values("value").annotate(total_count=Sum("count")).values("value", "total_count").order_by('date'))
+        appearences = (SearchApps.objects.filter(user=user, date__range=[start_date, today])
+            .annotate(truncated_date=TruncDate("date"))
+            .values("value", "truncated_date")
+            .annotate(total_count=Sum("count"))
+            .order_by("truncated_date")
+        )
     elif type == "month":
         start_date = today - timedelta(days=30) 
         appearences = (SearchApps.objects.filter(user=user, date__range=[start_date, today]).annotate(day_of_month=ExtractDay('date'), date_range=Case(When(day_of_month__lte=10, then=1), When(day_of_month__lte=20, then=2), When(day_of_month__lte=31, then=3), output_field=IntegerField(),)).values('date_range').annotate(total_count=Sum('count')).order_by('date_range'))
