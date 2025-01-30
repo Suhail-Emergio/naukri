0dcd97eba3358c93537054f93cc1cd4a97e30c3c
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-01-30 13:16:12 +0530

    fix: add delete_job endpoint to handle job deletion for logged users

--- jobs/jobposts/views.py
+++ jobs/jobposts/views.py
@@ -45,6 +45,13 @@ async def jobs(request):
     job = [i async for i in JobPosts.objects.filter(company__user=request.auth)]
     return 200, job
 
+@jobs_api.delete("/delete_job", response={200: List[JobData], 409: Message}, description="Job data passing of logged user")
+async def delete_job(request, id: str):
+    if await JobPosts.objects.filter(id=id).aexists():
+        job = await JobPosts.objects.get(id=id)
+        await job.adelete()
+    return 404, {"message": "Job data not found"}
+
 @jobs_api.get("/all_jobs", response={200: List[JobCompanyData], 409: Message}, description="Job data passing of all posts with respective company details")
 async def all_jobs(request):
     excludable_data = []
