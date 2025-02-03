8e02c83818d4ba7a237849bdbceb1e10fb4d3e73
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-03 15:50:21 +0530

    fix: update notification count retrieval to handle multiple users and improve accuracy

--- common_actions/signals.py
+++ common_actions/signals.py
@@ -12,8 +12,12 @@ from jobs.job_actions.models import ApplyJobs
 @receiver([post_save], sender=Notification)
 @receiver([post_save], sender=InviteCandidate)
 def update_counts(sender, instance, **kwargs):
-    user = instance.candidate.user if sender == InviteCandidate else instance.user
-    notification_count = user.notifications.exclude(read_by=user).count()
+    if sender == InviteCandidate:
+        users = [instance.candidate.user]
+    else:  # Notification
+        # Since user is M2M, get all related users
+        users = instance.user.all()
+    notification_count = Notification.objects.filter(user=user).exclude(read_by=user).count()
     if user.role == "seeker":
         invitation_count = InviteCandidate.objects.filter(candidate__user=user, read=False).count()
         message = {
