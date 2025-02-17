ef9165c53b8a8f6d9ed4654f8f8617201cbba59b
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-17 14:15:30 +0530

    feat: register CompanyDetails model in admin site

--- recruiter/admin.py
+++ recruiter/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from recruiter.recruiter_actions.models import InterviewSchedule
+from recruiter.company.models import CompanyDetails
 
+admin.site.register(CompanyDetails)
 admin.site.register(InterviewSchedule)
