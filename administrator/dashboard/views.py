e637a47b71b6f55d68e958e0cfe0d9cbae770bb7
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-28 13:16:19 +0530

    fix: update profile image retrieval to use URL in get_new_applications

--- administrator/dashboard/views.py
+++ administrator/dashboard/views.py
@@ -101,7 +101,7 @@ def get_new_applications():
         image = None
         if Personal.objects.filter(user=user).exists():
             personal = Personal.objects.get(user=user)
-            image = personal.profile_image
+            image = personal.profile_image.url
         apps.append({
             "name": name,
             "image": image
