b5886f1472d3431ed5188ee0d427253040727dea
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-13 15:00:39 +0530

    fix: uncomment notification logic in job posts expiry command

--- user/management/commands/job_posts_expiry.py
+++ user/management/commands/job_posts_expiry.py
@@ -12,11 +12,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         today = timezone.now().date()
         for j in NotificationPreference.objects.all():
-            noti_day = today.weekday() == 5 if j.alerts == "weekly" else True if j.alerts == "daily" else None
-            if noti_day:
-                subject="Reminder: Your job posts' validity is approaching its expiration. To ensure your listings remain active and visible to potential candidates, contact the admin to extend the validity. Don’t miss out on attracting top talent — renew your plan today!"
-                if JobPosts.objects.filter(expire_on__lte=today, company__user=j.user).exists():
-                    if j.user.onesignal_id and j.mobile_notifications:
-                        send_notifications(onesignal_id=j.user.onesignal_id, subject=subject, title="Job posts updations")
-                    if j.user.whatsapp_updations:
-                        send_updates(body=subject, number=j.user.phone)
+            # noti_day = today.weekday() == 5 if j.alerts == "weekly" else True if j.alerts == "daily" else None
+            # if noti_day:
+            subject="Reminder: Your job posts' validity is approaching its expiration. To ensure your listings remain active and visible to potential candidates, contact the admin to extend the validity. Don’t miss out on attracting top talent — renew your plan today!"
+            if JobPosts.objects.filter(expire_on__lte=today, company__user=j.user).exists():
+                if j.user.onesignal_id and j.mobile_notifications:
+                    send_notifications(onesignal_id=j.user.onesignal_id, subject=subject, title="Job posts updations")
+                if j.user.whatsapp_updations:
+                    send_updates(body=subject, number=j.user.phone)
