c04226c6714b735454e76d0eeb5f268919f9bd3b
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-13 14:50:50 +0530

    feat: register JobPosts model in Django admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from jobs.job_actions.models import ApplyJobs
+from jobs.jobposts.models import JobPosts
 
 admin.site.register(ApplyJobs)
+admin.site.register(JobPosts)
