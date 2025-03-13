e092cb91dddd5556934eb82449e9bd5f0adfe784
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-13 15:22:33 +0530

    feat: register Personal model in Django admin

--- seeker/admin.py
+++ seeker/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from seeker.details.models import SearchApps
+from seeker.details.models import SearchApps, Personal
 
 admin.site.register(SearchApps)
+admin.site.register(Personal)
