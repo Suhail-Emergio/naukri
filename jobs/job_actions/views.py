bc8d78b1f97785e6b049d5fd119dfdb8f80aff62
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-19 15:56:41 +0530

    fix: streamline location filtering in job applications query by removing unnecessary async call

--- jobs/job_actions/views.py
+++ jobs/job_actions/views.py
@@ -78,8 +78,8 @@ async def job_applications(request,
     else:
         query = Q(job__company__user=user)
     if location:
-        locations = await sync_to_async(Personal.objects.filter)(city=location)
-        query &= Q(user__in=[i.user async for i in locations])
+        # locations = await sync_to_async(Personal.objects.filter)(city=location)
+        query &= Q(user__in=[i.user async for i in Personal.objects.filter(city=location)])
         # query &= Q(user__personal__city=location)
     if gender:
         query &= Q(user__personal__gender=gender)
