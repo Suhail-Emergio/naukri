cf68c03b2231a5ade79e51de505f3dc36a7aa995
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-27 09:52:35 +0530

    fix: reorder fields in JobCreation schema and add interview_rounds and professional experience fields

--- jobs/jobposts/schema.py
+++ jobs/jobposts/schema.py
@@ -8,8 +8,8 @@ from recruiter.company.schema import CompanyCreation
 class JobCreation(Schema):
     title : str
     description : str
-    country : str
     city : str
+    country : str
     location_type : List[str]
     address : List[str] | None = None
     type : List[str]
@@ -24,16 +24,19 @@ class JobCreation(Schema):
     supplimental_pay : Optional[Union[List[str], Dict[str, Any]]] = None
     application_updations_email : Optional[Union[List[str], Dict[str, Any]]] = None
     resume_required : Optional[bool] = None
+    end_date : Optional[date] = None
     skills : Optional[Union[List[str], Dict[str, Any]]] = None
-    # experience : Optional[float] = None
+    interview_rounds : Optional[Union[List[str], Dict[str, Any]]] = None
+    professional_exp : Optional[float] = None
+    total_experience : Optional[float] = None
     education : Optional[str] = None
     custom_qns : Optional[Union[List[str], Dict[str, Any]]] = None
     languages : Optional[Union[List[str], Dict[str, Any]]] = None
+    work_location: Optional[Union[List[str], Dict[str, Any]]] = None
     commute : Optional[bool] = None
     relocate : Optional[bool] = None
     date_availablity : Optional[bool] = None
     gender : Optional[str] = None
-    end_date : Optional[date] = None
 
 JobData = create_schema(JobPosts)
 
