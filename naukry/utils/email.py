7c865150b21bacc8df9dd8eb9b740e5c026f8b83
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-04-02 12:49:15 +0530

    fix: correct email content variable in send_mails function

--- naukry/utils/email.py
+++ naukry/utils/email.py
@@ -7,7 +7,7 @@ from asgiref.sync import sync_to_async
 
 async def send_mails(email, content, subject, html):
     username = email
-    msg = EmailMultiAlternatives(subject, text, settings.EMAIL_HOST_USER, [email])
+    msg = EmailMultiAlternatives(subject, content, settings.EMAIL_HOST_USER, [email])
     msg.attach_alternative(html, "text/html")
     await sync_to_async(msg.send)()
     return {"status": True,"message" :"Email sended successfully"}
