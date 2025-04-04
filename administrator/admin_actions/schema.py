22fcf9fc2533454395add64bb50a9ea0635683a6
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-04-04 15:46:28 +0530

    refactor: remove unused contact fields from AdminCompany schema and update company details response

--- administrator/admin_actions/schema.py
+++ administrator/admin_actions/schema.py
@@ -39,12 +39,8 @@ class AdminCompany(Schema):
     functional_area : str
     address : Union[List[str], Dict[str, Any]]
     city : str
-    country : str
+    state : str
     postal_code : str
-    contact_name : str
-    contact_land_number : str
-    contact_mobile_number : str
-    designation : str
     logo: str | None
     id: int
     jobs: List[JobData]
--- administrator/admin_actions/views.py
+++ administrator/admin_actions/views.py
@@ -159,7 +159,7 @@ def all_company(request):
     companies = []
     for i in CompanyDetails.objects.all():
         jobs = JobPosts.objects.filter(company=i)
-        companies.append({"name": i.name, "about": i.about, "website": i.website, "functional_area": i.functional_area, "address": i.address, "city": i.city, "state": i.state, "postal_code": i.postal_code, "contact_name": i.contact_name, "contact_land_number": i.contact_land_number, "contact_mobile_number": i.contact_mobile_number, "designation": i.designation, "logo": i.logo, "id": i.id, "jobs": jobs})
+        companies.append({"name": i.name, "about": i.about, "website": i.website, "functional_area": i.functional_area, "address": i.address, "state": i.state, "postal_code": i.postal_code, "logo": i.logo, "id": i.id, "jobs": jobs})
     return companies
 
 #################################  S E E K E R S  #################################
