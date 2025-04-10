3f579c8930640dba5fefe010a05a57f5e96f50a8
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-04-10 18:16:45 +0530

    refactor: change edit_notifications endpoint from PATCH to POST method

--- administrator/admin_actions/views.py
+++ administrator/admin_actions/views.py
@@ -346,7 +346,7 @@ def all_notifications(request):
         return notifications
     return {"message" : "You are not authorized to access notifications"}
 
-@admin_api.patch("/edit_notifications", response={200: Message, 409:Message}, description="Edit Notifications")
+@admin_api.post("/edit_notifications", response={200: Message, 409:Message}, description="Edit Notifications")
 def edit_notifications(request, id: str, data: NotiData, image: File[UploadedFile]):
     logged_user = request.auth
     if logged_user.is_superuser:
